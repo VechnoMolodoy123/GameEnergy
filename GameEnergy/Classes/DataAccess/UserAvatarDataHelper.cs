c1076720f98fe70ca6830057a4920bcb3169fdfe
VechnoMolodoy123 <gabar1186@mail.ru>
2025-11-19 23:44:23 +0300

    Made a method for loading a user's avatar

---
+++ GameEnergy/Classes/DataAccess/UserAvatarDataHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEnergy.Classes.DataAccess
+{
+    internal class UserAvatarDataHelper
+    {
+        public static string GetBase64ImageFromDatabase(int currentUserAvatar)
+        {
+            var currentUser = Program.context.Users.FirstOrDefault(user => user.UserID == currentUserAvatar);
+
+            if (currentUser != null)
+            {
+                return currentUser.Avatar;
+            }
+
+            return null;
+        }
+    }
+}
--- GameEnergy/Classes/Images/InstallingImages/ImageLoader.cs
+++ GameEnergy/Classes/Images/InstallingImages/ImageLoader.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GameEnergy.Classes.DataAccess;
+using GameEnergy.Classes.Images.StoreImages;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
@@ -31,6 +33,23 @@ namespace GameEnergy.Classes.Images.InstallingImages
                 string logPath = "errorBookImages_log.txt";
                 File.AppendAllText(logPath, $"[{DateTime.Now}] Ошибка при загрузке изображения: {ex.Message}\n");
                 return Properties.Resources.DefaultGameImage;
+            }
+        }
+
+        public static void LoadAvatarImage(PictureBox avatarImage)
+        {
+            int currentUserId = Program.CurrentUser.UserID;
+
+            try
+            {
+                string base64Image = UserAvatarDataHelper.GetBase64ImageFromDatabase(currentUserId);
+
+                CodingOrDecoding.SetImageFromBase64(avatarImage, base64Image);
+            }
+            catch (Exception ex)
+            {
+                string logPath = "errorUserImages.txt";
+                File.AppendAllText(logPath, $"[{DateTime.Now}] Ошибка при загрузке изображения из базы данных:{ex.Message}\n");
             }
         }
     }
