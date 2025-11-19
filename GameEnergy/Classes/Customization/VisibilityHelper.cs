b3cbba2823c22ad533af955583f6c26c07e0e0ee
VechnoMolodoy123 <gabar1186@mail.ru>
2025-11-19 13:28:42 +0300

    Add VisibilityHelper class

---
+++ GameEnergy/Classes/Customization/VisibilityHelper.cs
@@ -0,0 +1,35 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameEnergy.Classes.Customization
+{
+    internal class VisibilityHelper
+    {
+        public static void ShowPassword(MaterialSingleLineTextField[] passwordFields, PictureBox showButton, PictureBox hideButton)
+        {
+            showButton.Visible = false;
+            hideButton.Visible = true;
+
+            foreach (var field in passwordFields)
+            {
+                field.PasswordChar = '\0';
+            }
+        }
+
+        public static void HidePassword(MaterialSingleLineTextField[] passwordFields, PictureBox showButton, PictureBox hideButton)
+        {
+            hideButton.Visible = false;
+            showButton.Visible = true;
+
+            foreach (var field in passwordFields)
+            {
+                field.PasswordChar = '*';
+            }
+        }
+    }
+}
