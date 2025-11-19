a06ceedf7481bb84e91e074fd95305eb31f5a1a8
VechnoMolodoy123 <gabar1186@mail.ru>
2025-11-19 23:35:43 +0300

    Add LoadBookImage method

--- GameEnergy/Classes/Images/InstallingImages/ImageLoader.cs
+++ GameEnergy/Classes/Images/InstallingImages/ImageLoader.cs
@@ -11,6 +11,27 @@ namespace GameEnergy.Classes.Images.InstallingImages
 {
     internal class ImageLoader
     {
-        
+        private static readonly string _path = "C:\\Users\\lamki\\OneDrive\\Документы\\GameEnergyRes\\";
+
+        public static Image LoadBookImage(string imagePath)
+        {
+            string fullImagePath = _path + imagePath;
+
+            if (string.IsNullOrEmpty(fullImagePath) || !File.Exists(fullImagePath))
+            {
+                return Properties.Resources.DefaultGameImage;
+            }
+
+            try
+            {
+                return Image.FromFile(fullImagePath);
+            }
+            catch (Exception ex)
+            {
+                string logPath = "errorBookImages_log.txt";
+                File.AppendAllText(logPath, $"[{DateTime.Now}] Ошибка при загрузке изображения: {ex.Message}\n");
+                return Properties.Resources.DefaultGameImage;
+            }
+        }
     }
 }
