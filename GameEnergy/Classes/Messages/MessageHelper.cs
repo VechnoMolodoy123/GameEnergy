5b1c72bc24b9409ac644ab4c088cc7eb160b12ca
VechnoMolodoy123 <gabar1186@mail.ru>
2025-11-19 13:29:48 +0300

    Add MessageHelper class

---
+++ GameEnergy/Classes/Messages/MessageHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameEnergy.Classes.Messages
+{
+    internal class MessageHelper
+    {
+        public static void ShowErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void ShowCustomTitleErrorMessage(string message, string caption)
+        {
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void ShowInformationMessage(string message, string caption)
+        {
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
