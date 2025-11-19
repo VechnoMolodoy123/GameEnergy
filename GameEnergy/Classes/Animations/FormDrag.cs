9210de364b87955b463bdfe176586910de346fef
VechnoMolodoy123 <gabar1186@mail.ru>
2025-11-19 13:27:02 +0300

    Add FormDrag class

---
+++ GameEnergy/Classes/Animations/FormDrag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameEnergy.Classes.Animations
+{
+    internal class FormDrag
+    {
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public static void DragingForm(Form form)
+        {
+            ReleaseCapture();
+            form.Opacity = 0.7;
+
+            SendMessage(form.Handle, 0x112, 0xf012, 0);
+            form.Opacity = 1;
+        }
+    }
+}
